8d0c57ef996a1d03dcbabb728b09bf22f7677892
MisterT <tbiffaud@gmail.com>
2019-04-12 17:23:15 +0200

    Initial commit

---
+++ Assets/Scripts/GameDataReader.cs
@@ -0,0 +1,53 @@
+﻿using System.IO;
+using UnityEngine;
+
+public class GameDataReader
+{
+    private BinaryReader _reader;
+    public int Version { get; }
+
+    public GameDataReader (BinaryReader reader, int version)
+    {
+        this._reader = reader;
+        this.Version = version;
+    }
+
+    public float ReadFloat()
+    {
+        return _reader.ReadSingle();
+    }
+
+    public int ReadInt()
+    {
+        return _reader.ReadInt32();
+    }
+
+    public Quaternion ReadQuaternion()
+    {
+        Quaternion value;
+        value.x = _reader.ReadSingle();
+        value.y = _reader.ReadSingle();
+        value.z = _reader.ReadSingle();
+        value.w = _reader.ReadSingle();
+        return value;
+    }
+
+    public Vector3 ReaderVector3()
+    {
+        Vector3 value;
+        value.x = _reader.ReadSingle();
+        value.y = _reader.ReadSingle();
+        value.z = _reader.ReadSingle();
+        return value;
+    }
+
+    public Color ReaderColor()
+    {
+        Color value;
+        value.r = _reader.ReadSingle();
+        value.g = _reader.ReadSingle();
+        value.b = _reader.ReadSingle();
+        value.a = _reader.ReadSingle();
+        return value;
+    }
+}
---
+++ Assets/Scripts/Shape.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shape : PersistableObject
+{
+    private Color _color;
+    private int _shapeId = int.MinValue;
+    private MeshRenderer _meshRenderer;
+    static int colorPropertyId = Shader.PropertyToID("_Color");
+    static MaterialPropertyBlock sharedPropertyBlock;
+
+    private void Awake()
+    {
+        _meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    public int ShapeId
+    {
+        get { return _shapeId; }
+        set
+        {
+            if (_shapeId == int.MinValue && value != int.MinValue)
+                _shapeId = value;
+            else
+                Debug.LogError("Not allowed to changed shapeId");
+        }
+    }
+
+    public int MaterialId { get; private set; }
+
+    public void SetMaterial (Material material, int materialId)
+    {
+        _meshRenderer.material = material;
+        MaterialId = materialId;
+    }
+
+    public void SetColor (Color color)
+    {
+        this._color = color;
+        if (sharedPropertyBlock == null)
+        {
+            sharedPropertyBlock = new MaterialPropertyBlock();
+        }
+        sharedPropertyBlock.SetColor(colorPropertyId, color);
+        _meshRenderer.SetPropertyBlock(sharedPropertyBlock);
+    }
+
+
+    public override void Save(GameDataWriter writer)
+    {
+        base.Save(writer);
+        writer.Write(_color);
+    }
+
+    public override void Load(GameDataReader reader)
+    {
+        base.Load(reader);
+        SetColor(reader.Version > 0 ? reader.ReaderColor() : Color.white);
+    }
+
+}
